c5c9d03971cc119a599a99d08f2800f70e60011d
Robert Jonczy <robert.jonczy@gmail.com>
2024-03-14 21:33:47 +0100

    dc-main consul

---
+++ dc-main/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# ----------------------------------------------------
+BIND="127.0.0.1"
+RESULTS_DIR=${1:-"~/consuldata"}
+DATADIR=$(mktemp -d ${RESULTS_DIR}/consul.XXXXXXXXXX)
+CONFIG_FILE="config.json"
+DC="dc-main"
+LOG_LEVEL="DEBUG"
+# ----------------------------------------------------
+
+# copy config to tmp datadir
+cp ${CONFIG_FILE} ${DATADIR}/${CONFIG_FILE}
+
+# start agent
+consul agent \
+  -dev \
+  -datacenter "${DC}" \
+  -bind "${BIND}" \
+  -config-file "${DATADIR}/${CONFIG_FILE}" \
+  -data-dir "${DATADIR}" \
+  -log-level "${LOG_LEVEL}"
