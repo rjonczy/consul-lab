58cc1268cfcabf74695cc4111b1aecba6eb83a29
Robert Jonczy <robert.jonczy@gmail.com>
2024-03-22 10:19:28 +0100

    http

--- dc-cluster-main/add-keys.sh
+++ dc-cluster-main/add-keys.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# ./add-keys.sh 10.0.0.5:8300
+# ./add-keys.sh 10.0.0.5:8500
 
-CONSUL_RPC_ADDR=${1:-"127.0.0.1:8500"}
-CONSUL_URL="http://${CONSUL_RPC_ADDR}/v1/kv"
+CONSUL_HTTP_ADDR=${1:-"127.0.0.1:8500"}
+CONSUL_URL="http://${CONSUL_HTTP_ADDR}/v1/kv"
 KEY_PREFIX="global/"
 
 COUNTER=1
@@ -13,7 +13,7 @@ while true; do
   
   KEY_NAME="${KEY_PREFIX}${COUNTER}"
   KEY_VALUE="This is value ${COUNTER}"
-  docker exec -it consul-server-0 curl -L -X PUT -d "${KEY_VALUE}" "${CONSUL_URL}/${KEY_NAME}"
+  curl -L -X PUT -d "${KEY_VALUE}" "${CONSUL_URL}/${KEY_NAME}"
 
   echo "Added key ${KEY_NAME} with value: ${KEY_VALUE}"
 
