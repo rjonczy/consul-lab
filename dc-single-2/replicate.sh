fd932e2b6e16293de87197ea466914cd049f5ce6
Robert Jonczy <robert.jonczy@gmail.com>
2024-03-22 10:07:55 +0100

    use correct dc names

--- dc-single-2/replicate.sh
+++ dc-single-2/replicate.sh
@@ -9,5 +9,5 @@ CONSUL_ADDR=${1:-"127.0.0.1:8500"}
 
 consul-replicate \
   -consul-addr ${CONSUL_ADDR} \
-  -prefix "global@dc-cluster-main:backup" \
+  -prefix "global@main-dc:backup" \
   -log-level "${LOG_LEVEL}"
